d97d8d36f6bd25de7be9f7dbbc3a0a06e11718dc
Osmolovsky95 <Sasha17121995>
2019-10-07 22:47:03 +0300

    Исправлено,чего не сделаешь ради 10 :)

--- src/homeTaskThree/HomeTaskThree.java
+++ src/homeTaskThree/HomeTaskThree.java
@@ -2,19 +2,26 @@ package homeTaskThree;
 
 
 
+
 public class HomeTaskThree {
     public static void main(String[] args) {
       oneOne();
-      oneTwo(7);
-      oneThree(666);
-        twoOne();
-        twoTwo();
-        twoThree();
-        three();
+      int [] j = new int [args.length];
+        for (int i = 0; i < args.length; i++) {
+            j[i] = Integer.parseInt(args[i]);
+        }
+      oneTwo(j[0]);
+      oneThree(j[0]);
+      twoOne();
+      twoTwo();
+      twoThree();
+      three();
 
     }
     //Задание 1.1
     public static void oneOne(){
+        System.out.println();
+        System.out.println("ЗАДАНИЕ 1.1");
         int [] array=new int[10];
         for (int i=1;i<array.length;i++){
             for (int j=1;j<=array.length;j++){
@@ -25,6 +32,8 @@ public class HomeTaskThree {
     }
     //Задание 1.2
     public static void oneTwo(int number){
+        System.out.println();
+        System.out.println("ЗАДАНИЕ 1.2");
         for (int i=1;i<=number;i++){
             for (int j=1;j<=number;j++){
                 System.out.println(i+" * "+j+" = "+i*j);
@@ -34,6 +43,9 @@ public class HomeTaskThree {
 
     //Задание 1.3
     public static void oneThree(int number){
+        System.out.println();
+        System.out.println("ЗАДАНИЕ 1.3");
+        System.out.println("Получено число: "+number);
         String lenght=""+number;
         int sum=1;
        int massiv[]=new int[lenght.length()];
@@ -50,8 +62,11 @@ public class HomeTaskThree {
 
     //Задание 2.1
     public static void twoOne(){
+        System.out.println();
+        System.out.println("ЗАДАНИЕ 2.1");
         int [] array={5,2,3,45,67,7,8,8,312,99,100};
         int i=0;
+        System.out.println("Цикл do");
         //do-while
         do {
 
@@ -60,16 +75,19 @@ public class HomeTaskThree {
         }
         while (i<array.length);
        //while
+        System.out.println("Цикл while");
        i=0;
        while (i<array.length){
            System.out.println(array[i]);
            i++;
        }
        //for
+        System.out.println("Цикл for");
        for (i=0;i<array.length;i++){
            System.out.println(array[i]);
        }
        //foreach
+        System.out.println("Цикл foreach");
        for(int s:array){
            System.out.println(s);
        }
@@ -77,6 +95,8 @@ public class HomeTaskThree {
 
     //Задание 2.2
     public static void twoTwo(){
+        System.out.println();
+        System.out.println("ЗАДАНИЕ 2.2");
         int [] array=new int[70];
         for (int i=0;i<array.length;i++){
             array[i]=(int)(Math.random()*40);
@@ -87,6 +107,8 @@ public class HomeTaskThree {
     }
   //Задание 2.3
     public static void twoThree(){
+        System.out.println();
+        System.out.println("ЗАДАНИЕ 2.3");
         Test [] array=new Test[10];
         int i=0;
         int a=2;
@@ -107,6 +129,8 @@ public class HomeTaskThree {
     //Задание 3
 
     public static void three(){
+        System.out.println();
+        System.out.println("ЗАДАНИЕ 3");
        Car[]cars = new Car[30];
        for (int i=0;i<cars.length;i++){
           if (i%2!=0){
