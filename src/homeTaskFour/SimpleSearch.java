be9016ac1f5875bd0cb910ee6c53efb2f31df9a1
Osmolovsky95 <Sasha17121995>
2019-10-10 23:15:36 +0300

    Main находится в homeTaskFour,не могу понять как тут применить полиморфизм :(

--- src/homeTaskFour/SimpleSearch.java
+++ src/homeTaskFour/SimpleSearch.java
@@ -12,8 +12,6 @@ public class SimpleSearch implements IRobot {
         int index = 0;
         index = warAndWorld.indexOf("война");
         String[] massiv2 = warAndWorld.split(" +");
-        String str = "война война белеберда саша война война";
-        String[] massiv3 = str.split(" +");
         int j=0;
         for (int i = 0; i < massiv2.length; i++) {
             if (massiv2[i].equals("война")) {
